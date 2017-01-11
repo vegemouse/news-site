dd6f323fe07b187c102513facbd41078f56e1fff
Maxwell Cady and Nigel Burnett <maxwell@>
2017-01-10 16:22:03 -0800

    add styling to articles

--- app/routes/hacker-news.js
+++ app/routes/hacker-news.js
@@ -2,7 +2,7 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model() {
-    return $.get("https://newsapi.org/v1/articles?source=hacker-news&sortBy=latest&apiKey=9d962b0723554d7fa068b02b75274e7b").then(function(response) {
+    return $.get("https://newsapi.org/v1/articles?source=polygon&sortBy=top&apiKey=9d962b0723554d7fa068b02b75274e7b").then(function(response) {
       return response;
     });
   },
